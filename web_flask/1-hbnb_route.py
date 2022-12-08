19a7781fb140d8d838c32ac5dc1c4b6847674b42
mustapha muhammad <ibrahimmusty.me@gmail.com>
2022-12-08 06:20:37 +0000

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -15,5 +15,11 @@ def index():
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb')
+def hbnb():
+    '''The hbnb page.'''
+    return 'HBNB'
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000')`
