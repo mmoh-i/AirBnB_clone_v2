4a88089e8d2870f73e651542106a6ce3ce0d4168
mustapha muhammad <ibrahimmusty.me@gmail.com>
2022-12-08 06:27:35 +0000

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,5 +21,11 @@ def hbnb():
     return 'HBNB'
 
 
+@app.route('/c/<text>')
+def c_page(text):
+    '''The c page.'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')`
+    app.run(host='0.0.0.0', port='5000')
